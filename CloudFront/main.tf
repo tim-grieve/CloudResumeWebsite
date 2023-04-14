7eee6d41a1f1b5b9a58200cd0e68fb972f4900b5
Tim <tim.grieve@shellflow.com>
2023-04-14 15:08:10 -0400

    Adjusted viewer certificate settings

--- CloudFront/main.tf
+++ CloudFront/main.tf
@@ -133,5 +133,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   viewer_certificate {
     acm_certificate_arn = aws_acm_certificate.default.arn
+    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv2"
   }
 }
