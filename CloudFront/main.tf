341adc48e269e72a8f4d24b44b62d34ca0eb6480
Tim <tim.grieve@shellflow.com>
2023-04-14 15:02:37 -0400

    updated acm cert config for viewer certificate

--- CloudFront/main.tf
+++ CloudFront/main.tf
@@ -132,6 +132,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = aws_acm_certificate.default.arn
+    acm_certificate_arn = aws_acm_certificate.default.arn
   }
 }
