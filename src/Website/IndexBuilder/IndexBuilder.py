af4279d48d5bb43391c6995e063b484b28463acb
Tim <tim.grieve@shellflow.com>
2023-07-19 12:19:45 -0400

    testing IndexBuilderCode in git hub actions runner step by step

--- src/Website/IndexBuilder/IndexBuilder.py
+++ src/Website/IndexBuilder/IndexBuilder.py
@@ -6,22 +6,23 @@ with open ('content.json') as json_file:
     content = json.load(json_file)
 
 #Build HTML file for website
-environment = Environment(loader=FileSystemLoader("./templates/"))
-resume_filename = "../index.html"
-resume_template = environment.get_template("resume.html")
+environment = Environment(loader=FileSystemLoader("templates/"))
+print(environment)
+#resume_filename = "../index.html"
+#resume_template = environment.get_template("resume.html")
 
-context = {
-    "resume_name": content['details']['name'],
-    "email": content['details']['email'],
-    "linkedin": content['details']['linkedinurl'],
-    "github": content['details']['githuburl'],
-    "jobs": content['experience'],
-    "certs": content['certifications'],
-    'projects': content['projects']
-}
+#context = {
+#    "resume_name": content['details']['name'],
+#    "email": content['details']['email'],
+#    "linkedin": content['details']['linkedinurl'],
+#    "github": content['details']['githuburl'],
+#    "jobs": content['experience'],
+#    "certs": content['certifications'],
+#    'projects': content['projects']
+#}
 
-with open(resume_filename, mode="w", encoding="utf-8") as resume:
-    resume.write(resume_template.render(context))
+#with open(resume_filename, mode="w", encoding="utf-8") as resume:
+#    resume.write(resume_template.render(context))
 
 
 
