fca9d77521339856b1f02a236900cd3214a5c92e
Tim <tim.grieve@shellflow.com>
2023-07-19 09:33:24 -0400

    Added python code and html template to build index.html automatically from json input

---
+++ src/Website/IndexBuilder/IndexBuilder.py
@@ -0,0 +1,28 @@
+import json
+from jinja2 import Environment, FileSystemLoader
+
+#Store inputs in JSON file
+with open ('content.json') as json_file:
+    content = json.load(json_file)
+
+#Build HTML file for website
+environment = Environment(loader=FileSystemLoader("templates/"))
+resume_filename = "index.html"
+resume_template = environment.get_template("resume.html")
+
+context = {
+    "resume_name": content['details']['name'],
+    "email": content['details']['email'],
+    "linkedin": content['details']['linkedinurl'],
+    "github": content['details']['githuburl'],
+    "jobs": content['experience'],
+    "certs": content['certifications'],
+    'projects': content['projects']
+}
+
+with open(resume_filename, mode="w", encoding="utf-8") as resume:
+    resume.write(resume_template.render(context))
+
+
+
+
